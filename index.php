c5c1b5f19d3543e4e398d3900007f4c8fb4ef600
Kevin_Ramirez <stalram98@gmail.com>
2020-08-12 14:21:03 -0500

    index-about

--- index.php
+++ index.php
@@ -19,23 +19,31 @@
 </head>
 
 <body>
+    <?php
+    $conexion = mysqli_connect("localhost", "root", "kevinroot", "tiendaropa2022") or 
+    die("Problemas con la conexión");
+
+    $registros = mysqli_query($conexion, "SELECT eslogan FROM `Pagina`") or
+    die("Problemas en el select:" . mysqli_error($conexion));
+
+    ?>
 
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="index.html">StyleShop</a>
+            <a class="navbar-brand" href="index.php">StyleShop</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.html">Home
+                        <a class="nav-link" href="index.php">Home
               <span class="sr-only">(current)</span>
             </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="about.html">About</a>
+                        <a class="nav-link" href="about.php">About</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="tienda.php">Products</a>
@@ -56,28 +64,22 @@
         <img class="img-fluid d-block mx-auto" src="http://placehold.it/200x200&text=Logo" alt="">
     </header>
 
-    <!-- Content section -->
+    <!-- Cont   ent section -->
     <section class="py-5">
         <div class="container">
-            <h1>Section Heading</h1>
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, suscipit, rerum quos facilis repellat architecto commodi officia atque nemo facere eum non illo voluptatem quae delectus odit vel itaque amet.</p>
-        </div>
+            <h1>Slogan</h1>
+            <?php
+            if ($reg = mysqli_fetch_array($registros)) {
+                echo '<p class="lead">'.$reg['eslogan'].'</p>';
+            }
+            ?>          
+            </div>
     </section>
-
-    <!-- <div class="py-5 bg-image-full" style="background-image: url('https://unsplash.it/1900/1080?image=1081');">
-        <div style="height: 200px;"></div>
-    </div> -->
-
-    <!-- Footer -->
     <footer class="py-5 bg-dark">
         <div class="container">
             <p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
         </div>
-        <!-- /.container -->
     </footer>
-
-    <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
