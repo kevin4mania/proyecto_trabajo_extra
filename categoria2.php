87a9b707fc9a6dff4544c1af25142afdcb766046
LAPTOP-D1I711DA\Lenovo 330S <nicolascarrasco98@hotmail.com>
2020-08-12 23:48:53 -0500

    Correccion Nombres

--- categoria2.php
+++ categoria2.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Shop Homepage - Start Bootstrap Template</title>
+    <title>Tienda</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -29,29 +29,31 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="index.php">Start Bootstrap</a>
+            <a class="navbar-brand" href="index.php">StyleShop</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
                 aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Home
-                            <span class="sr-only">(current)</span>
-                        </a>
-                    <li class="nav-item">
-                        <a class="nav-link" href="about.php">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="tienda.php">Products</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="servicios.html">Services</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contacts.html">Contact</a>
-                    </li>
+                <li class="nav-item active">
+
+                <a class="nav-link" href="index.php">Home
+                <span class="sr-only">(current)</span>
+                </a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="about.php">Quienes Somos</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="tienda.php">Tienda</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="servicios.php">Servicios</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="contacts.php">Contáctanos</a>
+                </li>
 
                 </ul>
             </div>
